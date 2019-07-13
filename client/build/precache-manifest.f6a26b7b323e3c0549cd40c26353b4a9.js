ec842b5fb2b7c3a377116a28ea77a6a1908bd9ed
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-07-13 02:50:51 +0000

    added build files

---
+++ client/build/precache-manifest.f6a26b7b323e3c0549cd40c26353b4a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383ff5e3415aa16f0b653204347af7e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ad73a047b1908f42412",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "36b3287272cc4c697797",
+    "url": "./static/js/2.47009f54.chunk.js"
+  },
+  {
+    "revision": "2ad73a047b1908f42412",
+    "url": "./static/js/main.98e44757.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
